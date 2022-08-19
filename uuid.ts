a57599454f1cbfd46e62da339af757c74fff12ca
Ivan Sedykh <59384103+Ofigelov@users.noreply.github.com>
2022-08-19 11:48:09 +0500

    Update uuid.ts

--- uuid.ts
+++ uuid.ts
@@ -8,12 +8,19 @@ for (let i = 0; i < 256; ++i) {
 
 export const uuid4 = (): string => {
     const rnd = random(16);
+
     rnd[6] = (rnd[6] & 0x0f) | 0x40;
     rnd[8] = (rnd[8] & 0x3f) | 0x80;
-    const result: string[] = [];
+
+    let result = '';
+
     for (let i = 0; i < rnd.length; i++) {
-        result.push(byteToHex[rnd[i]]);
-        if (i === 3 || i === 5 || i === 7 || i === 9) result.push('-');
+        result += byteToHex[rnd[i]];
+
+        if (i === 3 || i === 5 || i === 7 || i === 9) {
+            result += '-';
+        }
     }
-    return result.join('');
+
+    return result;
 };
